5aaef9eb870afaf991356f32caf002de355b1fd9
Monique Aruna <monique.aruna@gmail.com>
2017-06-14 22:30:39 -0700

    Set up lavv-ios project.

---
+++ lavv-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  lavv-ios
+//
+//  Created by Monique Solanki on 6/14/17.
+//  Copyright © 2017 lavv. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
