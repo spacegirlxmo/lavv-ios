f5853ce1122c3860cafaa1278052c1b44fcd6748
Monique Aruna <monique.aruna@gmail.com>
2017-06-17 20:56:42 -0700

    Installed CocoaPods. Added Facebook login code + button.

--- lavv-ios/AppDelegate.swift
+++ lavv-ios/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let accessToken = AccessToken.current {
+            // User is logged in, use 'accessToken' here.
+            print("Access token: \(accessToken)")
+        } else {
+            print("No access token.")
+        }
         return true
     }
 
